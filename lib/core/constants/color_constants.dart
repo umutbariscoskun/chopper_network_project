b932e6b804f20de25e2866139d154481af655408
Umut <umutcoskun2017@gmail.com>
2024-10-08 00:17:12 +0300

    remove unnecessary colors

--- lib/core/constants/color_constants.dart
+++ lib/core/constants/color_constants.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 
 abstract class AppColors {
-  static const Color dodgerBlue = Color(0xff1FA2FF);
-  static const Color vividSkyBlue = Color(0xff12D8FA);
-  static const Color magicMint = Color(0xffA6FFCB);
   static const Color textColor = Color(0xff36394A);
   static const Color white = Colors.white;
 }
