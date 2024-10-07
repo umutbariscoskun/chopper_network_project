8bb5d7fec00a1ebebfbd5f08a4279d25b665464f
Umut <umutcoskun2017@gmail.com>
2024-10-08 00:16:23 +0300

    remove unnecessary locale and colors

--- lib/core/constants/color_constants.dart
+++ lib/core/constants/color_constants.dart
@@ -6,15 +6,4 @@ abstract class AppColors {
   static const Color magicMint = Color(0xffA6FFCB);
   static const Color textColor = Color(0xff36394A);
   static const Color white = Colors.white;
-
-  static const dodgerGradient2 = LinearGradient(
-    begin: Alignment(0.42, 1),
-    end: Alignment(1, 0.5),
-    colors: [
-      Color.fromRGBO(31, 163, 255, 1),
-      Color.fromRGBO(18, 217, 250, 1),
-      Color.fromRGBO(166, 255, 204, 1),
-    ],
-    stops: [0.00, 0.50, 1.00],
-  );
 }
